135a313455606784978f3ebfb1a5a3b8803988b5
Nyame1997 <ciaravillanueva1997@gmail.com>
2021-04-08 22:51:26 +0000

    Add courses model and iterate through for index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.create(short_name: 'CS111', name: 'Intro to Computer Science', description: 'An introduction to Computer Science')
+Course.create(short_name: 'CS112', name: 'Computer Science Basics', description: 'Covers more advanced basics of Computer Science')
+Course.create(short_name: 'ENG100', name: 'Beginner English', description: 'Beginner Course in English and Writing')
