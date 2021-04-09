b5399a2d0abdc5eb9c3446d459db395a6cccf3df
Nyame1997 <ciaravillanueva1997@gmail.com>
2021-04-09 21:41:37 +0000

    Add login functionality and login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'courses#index'
   resources :courses, except: [:destroy, :show]
   get 'about', to: 'pages#about'
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
 end
